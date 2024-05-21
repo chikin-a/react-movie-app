c3819c7d093feb522ec9df35d63c89e15aa82834
dania <dquark74@protonmail.com>
2024-05-21 16:06:31 +0300

    added muted black bc to header if it overlays page content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,51 @@
-import { Link } from 'react-router-dom'
-import './Header.scss'
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import { useEffect } from "react";
 
 const Header = () => {
+  useEffect(() => {
+    // Added background to header from the point where it overlays page content
+    const handleScroll = () => {
+      const scrollPosition =
+        window.scrollY || document.documentElement.scrollTop;
+      const targetElement = document.querySelector(".header");
+      const threshold = 125;
+
+      if (targetElement) {
+        if (scrollPosition > threshold) {
+          targetElement.classList.add("scrolled-past-threshold");
+        } else {
+          targetElement.classList.remove("scrolled-past-threshold");
+        }
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className='header'>
-      <div className='container'>
-        <div className='header-container'>
-          <Link className='logo' to={'/'}>
-            <img src='/icons/logo.svg' alt='Logo' />
+    <header className="header">
+      <div className="container">
+        <div className="header-container">
+          <Link className="logo" to={"/"}>
+            <img src="/icons/logo.svg" alt="Logo" />
           </Link>
-          <nav className='navbar'>
-            <Link className='navbar__item' to={'/movies'}>
+          <nav className="navbar">
+            <Link className="navbar__item" to={"/movies"}>
               Movies
             </Link>
-            <Link className='navbar__item' to={'/tv-shows'}>
+            <Link className="navbar__item" to={"/tv-shows"}>
               TV Shows
             </Link>
-            <Link className='navbar__item suggest' to={'/suggest-me'}>
+            <Link className="navbar__item suggest" to={"/suggest-me"}>
               Suggest me
             </Link>
           </nav>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
